6eeda276fdae3dd5a0cb53c66cb4285c22066f4f
Alex Sampaio <alexsampaio.contato@gmail.com>
2024-11-01 14:44:30 -0300

    fix: icon to our version

--- src/plugins/async-enum/admin/src/components/PluginIcon.tsx
+++ src/plugins/async-enum/admin/src/components/PluginIcon.tsx
@@ -1,5 +1,5 @@
-import { PuzzlePiece } from '@strapi/icons';
+import { Puzzle } from '@strapi/icons';
 
-const PluginIcon = () => <PuzzlePiece />;
+const PluginIcon = () => <Puzzle />;
 
 export { PluginIcon };
