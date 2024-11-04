81caa051ff16736f596b64353914aef0e42ff11b
Alex Sampaio <alexsampaio.contato@gmail.com>
2024-11-04 09:10:13 -0300

    feat: saves changes selector

--- src/plugins/async-enum/admin/src/components/AsyncDropdown.tsx
+++ src/plugins/async-enum/admin/src/components/AsyncDropdown.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { DesignSystemProvider, lightTheme } from '@strapi/design-system';
 import axios from 'axios';
 
-const SelectorFieldTest: React.FC = () => {
+const SelectorFieldTest: React.FC<any> = ({ name, value, onChange }) => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
@@ -10,7 +11,6 @@ const SelectorFieldTest: React.FC = () => {
             try {
                 const response = await axios.get('https://jsonplaceholder.typicode.com/users');
                 setUsers(response.data);
-                console.log('HELLOOOOOOOOOOOOOOOOO', response.data);
             } catch (error) {
                 console.error('Error fetching users:', error);
             }
@@ -19,11 +19,16 @@ const SelectorFieldTest: React.FC = () => {
         fetchUsers();
     }, []);
 
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        onChange({ target: { name, value: event.target.value } });
+    };
+
     return (
         <DesignSystemProvider locale="en-GB" theme={lightTheme}>
             <div>
                 <h2>Select a User</h2>
-                <select>
+                <select value={value || ''} onChange={handleSelectChange}>
+                    <option value="" disabled>Select a user</option>
                     {users.map((user: any) => (
                         <option key={user.id} value={user.id}>
                             {user.name}
@@ -33,6 +38,16 @@ const SelectorFieldTest: React.FC = () => {
             </div>
         </DesignSystemProvider>
     );
+};
+
+SelectorFieldTest.defaultProps = {
+    value: '',
+};
+
+SelectorFieldTest.propTypes = {
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
 };
 
 export default SelectorFieldTest;
